6d387ba3962d9e052735f5366dfd9b1620a76296
Rithvik Vibhu <rithvikvibhu@gmail.com>
2025-12-08 12:42:07 +0530

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,19 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+  site: process.env.SITE_URL || 'https://blek.space',
+
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react(), mdx()],
+  integrations: [react(), mdx(), sitemap()],
 
   image: {
     layout: 'full-width'
